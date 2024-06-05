9d7b9ca9403b4d184732aa5ed44e99ab668b1433
Chris Stevenson <chrislstevenson@gmail.com>
2024-06-05 04:27:05 -0400

    Small progress bar template
    - had to look at solution vid for this one

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,33 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const ProgressBar = ({ value, size, label = 'Progress: ' }) => {
+  return (
+    <Wrapper
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+    >
+      <VisuallyHidden>{value}</VisuallyHidden>
+      <Bar value={value} />
+    </Wrapper>
+  )
 };
+
+const Wrapper = styled.div`
+  height: 8px;
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: 4px;
+  overflow: hidden;
+`;
+
+const Bar = styled.div`
+  width: ${({ value }) => value + '%'};
+  height: 8px;
+  background-color: ${COLORS.primary};
+  border-radius: 4px 0px 0px 4px;
+`;
 
 export default ProgressBar;
