4e43bb7429bc7de4d2fc9c47f74a312d37e367fc
Chris Stevenson <chrislstevenson@gmail.com>
2024-06-05 10:45:12 -0400

    Style select dropdown
    - best I could do before looking at solution vid
    - I can make the drop-down icon disappear but couldn't get the replacement to work

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,42 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
+import { Divide } from "react-feather";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <SelectWrapper displayed={displayedValue} value={value} onChange={onChange}>
       {children}
-    </select>
+    </SelectWrapper>
   );
 };
+
+const SelectWrapper = styled.select`
+  font-family: "Roboto", sans-serif;
+  font-size: 16px;
+  /* my attempt at dynamic sizing */
+  width: ${({ displayed }) => {
+    return displayed.length + "rem";
+  }};
+  /* appearance: none; */
+  position: relative;
+  background-color: ${COLORS.transparentGray15};
+  border: none;
+  padding: 12px;
+  border-radius: 8px;
+
+  &:hover {
+    font-weight: 700;
+  }
+
+  &:focus {
+    border: 2px solid ${COLORS.primary};
+  }
+`;
 
 export default Select;
