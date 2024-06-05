c5baadf48802ba960e689f79776beb38965c1044
Chris Stevenson <chrislstevenson@gmail.com>
2024-06-05 04:42:27 -0400

    Add medium and large variants of progress bar
    - this part I did on my own before watching the rest of the soln vid

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,7 +5,12 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size, label = 'Progress: ' }) => {
+const ProgressBar = ({ value, size = 'small', label = 'Progress: ' }) => {
+
+  let height = 8;
+  if (size.toLowerCase() === 'medium') height = 12;
+  if (size.toLowerCase() === 'large') height = 16;
+
   return (
     <Wrapper
       role="progressbar"
@@ -14,13 +19,12 @@ const ProgressBar = ({ value, size, label = 'Progress: ' }) => {
       aria-valuemax="100"
     >
       <VisuallyHidden>{value}</VisuallyHidden>
-      <Bar value={value} />
+      <Bar value={value} height={height} />
     </Wrapper>
   )
 };
 
 const Wrapper = styled.div`
-  height: 8px;
   background-color: ${COLORS.transparentGray15};
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
   border-radius: 4px;
@@ -29,7 +33,11 @@ const Wrapper = styled.div`
 
 const Bar = styled.div`
   width: ${({ value }) => value + '%'};
-  height: 8px;
+  height: ${({ height }) => height + 'px'};
+  margin: ${({ height }) => {
+    if (height === 16) return 4 + 'px';
+    return 0;
+  }};
   background-color: ${COLORS.primary};
   border-radius: 4px 0px 0px 4px;
 `;
